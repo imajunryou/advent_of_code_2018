271d2831082c78c4e5bf9b974ffbe66fd99befc2
Ima Junryou <imajunryou@gmail.com>
2018-12-20 07:17:30 -0700

    Reworked solution to (very slowly) find the first duplicate frequency.

--- day_1/frequency.py
+++ day_1/frequency.py
@@ -1,7 +1,9 @@
 import argparse
 import os
 import sys
+from typing import List, Tuple
 
+from ordered_set import OrderedSet
 
 def parse_frequency(frequency):
     result = 0
@@ -12,8 +14,19 @@ def parse_frequency(frequency):
     return result
 
 
-def update_frequency(current_frequency, next_frequency):
-    return current_frequency + next_frequency
+def update_frequency(frequency: 'OrderedSet[int]', next_frequency: int) -> 'Tuple[OrderedSet[int], int]':
+    result_frequency: 'OrderedSet[int]' = OrderedSet(frequency)
+    most_recent_frequency = result_frequency[-1]
+    subtotal = most_recent_frequency+next_frequency  # type: ignore  # indexing returned an int
+    result_frequency.add(subtotal)
+    return (result_frequency, subtotal)
+
+
+def has_repeat(initial: 'OrderedSet[int]', final: 'OrderedSet[int]') -> bool:
+    result = len(initial) == len(final)
+    if result:
+        print("Found a duplicate!")
+    return result
 
 
 def get_args(args):
@@ -40,6 +53,12 @@ def get_args(args):
             "The initial starting frequency.  Defaults to 0."
         )
     )
+    parser.add_argument(
+        "--repeat", action="store_true", help=(
+            "Aborts frequency updating and reports the most recent sum "
+            "after encountering the same subtotal twice."
+        )
+    )
     return parser.parse_args(args)
 
 
@@ -47,13 +66,33 @@ def get_input_path(args):
     return os.path.join(args.path, args.file)
 
 
+def find_repeat(current_frequency: 'OrderedSet[int]', frequencies: 'List[int]') -> int:
+    looping = True
+    subtotal = current_frequency[-1]  # type: ignore  # subtotal is an int
+    loops = 0
+    while looping:
+        loops += 1
+        for next_frequency in frequencies:
+            previous_frequency = OrderedSet(current_frequency)
+            current_frequency, subtotal = update_frequency(current_frequency, next_frequency)
+            if has_repeat(previous_frequency, current_frequency):
+                looping = False
+                break
+        print("Loop #{} - Size: {}".format(loops, len(current_frequency)))
+    return subtotal
+
+
 if __name__ == "__main__":
     args = get_args(sys.argv[1:])
-    current_frequency = args.initial
+    subtotal = args.initial
+    current_frequency = OrderedSet([subtotal])
     with open(get_input_path(args), 'r') as f:
-        for next_frequency in f:
-            current_frequency = update_frequency(
-                current_frequency,
-                parse_frequency(next_frequency)
-            )
-    print(current_frequency)
+        frequencies = [parse_frequency(next_frequency) for next_frequency in f]
+        if args.repeat == True:
+            print("Hunting for repeats")
+            subtotal = find_repeat(current_frequency, frequencies)
+        else:
+            print("Finding a sum")
+            subtotal = sum(frequencies)
+    print(subtotal)
+
--- day_1/test_frequency.py
+++ day_1/test_frequency.py
@@ -2,6 +2,8 @@
 
 import pytest
 
+from ordered_set import OrderedSet
+
 import frequency
 
 
@@ -22,10 +24,42 @@ def test_parse_frequency_handles_nonnumerics(given):
     assert frequency.parse_frequency(given) == 0
 
 def test_update_frequency_counts_up_with_positives():
-    assert frequency.update_frequency(0, 1) == 1
+    assert frequency.update_frequency(OrderedSet([0]), 1) == (OrderedSet([0, 1]), 1)
 
 def test_update_frequency_counts_down_with_negatives():
-    assert frequency.update_frequency(0, -1) == -1
+    assert frequency.update_frequency(OrderedSet([0]), -1) == (OrderedSet([0, -1]), -1)
 
 def test_update_frequency_unchanged_with_zeroes():
-    assert frequency.update_frequency(0, 0) == 0
+    assert frequency.update_frequency(OrderedSet([1]), 0) == (OrderedSet([1]), 1)
+
+def test_has_repeat_returns_false_without_repeat():
+    initial = OrderedSet([0, 1])
+    final, subtotal = frequency.update_frequency(initial, 1)
+    assert frequency.has_repeat(initial, final) == False
+
+def test_has_repeat_returns_true_with_repeat():
+    initial = OrderedSet([3, 2])
+    final, subtotal = frequency.update_frequency(initial, 1)
+    assert frequency.has_repeat(initial, final) == True
+
+sets = [
+    (OrderedSet([3]), -1, (OrderedSet([3, 2]), 2)),
+    (OrderedSet([3, 2]), 2, (OrderedSet([3, 2, 4]), 4)),
+    (OrderedSet([3, 2]), 1, (OrderedSet([3, 2]), 3)),
+    (OrderedSet([0, 3, 6, 10, 8]), -4, (OrderedSet([0, 3, 6, 10, 8, 4]), 4))
+]
+@pytest.mark.parametrize('past_frequencies, next_frequency, expected_frequencies', sets)
+def test_frequency_subtotals_are_insertion_ordered(past_frequencies, next_frequency, expected_frequencies):
+    assert frequency.update_frequency(past_frequencies, next_frequency) == expected_frequencies
+
+repeatables = [
+    (0, [3, 3, 4, -2, -4], 10),
+    (0, [1, -1], 0),
+    (0, [-6, 3, 8, 5, -6], 5),
+    (0, [7, 7, -2, -7, -4], 14)
+]
+@pytest.mark.parametrize('initial, frequencies, expected', repeatables)
+def test_repeating_updates_loops_until_results_found(initial, frequencies, expected):
+    initial_set = OrderedSet([initial])
+    assert frequency.find_repeat(initial_set, frequencies) == expected
+    
